4a76bd826ea69dabc4f132719b44d5f38965011e
99nk <99.niharikakumar@gmail.com>
2020-06-09 00:46:21 +0530

    Adding dialog box to inform admin about the Checking of User Credentials.

--- app/src/main/java/com/android/sgms_20/LoginActivity.java
+++ app/src/main/java/com/android/sgms_20/LoginActivity.java
@@ -155,12 +155,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch(v.getId()){
             case R.id.register_account_link:
                 startActivity(new Intent(this,RegisterActivity.class));
-                finish();
+                //finish();
                 break;
 
             case R.id.login_button:
                 UserLogin();
-                finish();
+                //finish();
                 break;
         }
     }
@@ -177,7 +177,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent=new Intent(LoginActivity.this,MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
-            finish();
+            //finish();
         }
         else
         {
